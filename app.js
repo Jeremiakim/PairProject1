9d318f87cdf6ac83579700d99ca18ded581d41f6
Jeremiakim <jeremikimzu67@gmail.com>
2023-09-28 13:03:04 +0700

    set up buat login register

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const router = require('./routers')
+const port = 3000 
+
+app.set('view engine', 'ejs')
+app.use (express.urlencoded({extended : true}))
+
+app.use(router)
+
+app.listen(port,()=>{
+    console.log(`Go to PORT ${port}`);
+})
+
