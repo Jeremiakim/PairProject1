485638469563b8457d6892c132a62054636ae770
Ngurah Wira <ngurahwira27@gmail.com>
2023-09-28 21:41:50 +0700

    sampai di home

--- controller/controller.js
+++ controller/controller.js
@@ -2,19 +2,39 @@ const {Package} = require("../models/");
 
 
 class Controller {
+  //home
   static showHome(req, res) {
-    console.log(typeof Package);
     Package.findAll()
-    .then((data) => {
-      // res.send(data)
-      console.log(data[0])
-      res.render("home", { data });
-    })
-    .catch((err) => {
-      res.send(err);
-    });
+      .then((data) => {
+        // res.send(data)
+        console.log(data[0]);
+        res.render("home", { data });
+      })
+      .catch((err) => {
+        res.send(err);
+      });
   }
-
+  //show form
+  static showForm(req, res) {
+    res.render("formPackage");
+  }
+  //add paket
+  // static addPackage(req, res) {
+  //   const { name, description, price, category, image } = req.body;
+  //   Package.create({
+  //     name,
+  //     description,
+  //     price,
+  //     category,
+  //     image
+  //   })
+  //     .then((_) => {
+  //       res.redirect("/");
+  //     })
+  //     .catch((err) => {
+  //       res.send(err);
+  //     });
+  // }
 }
 
 
--- router/index.js
+++ router/index.js
@@ -2,15 +2,10 @@ const Controller = require("../controller/controller");
 const router = require("express").Router();
 
 router.get("/", Controller.showHome);
-// router.get("/stores", Controller.showStore);
-// router.get("/stores/add", Controller.showFormStore);
-// router.post("/stores/add", Controller.addStore);
-// router.get("/stores/:storeId", Controller.showDetail)
-// router.get("/stores/:storeId/employees/add", Controller.showFormEm);
-// router.post("/stores/:storeId/employees/add", Controller.addEmployee);
-// router.get("/stores/:storeId/employees/:employeeId/edit", Controller.showFormEdit);
-// router.post("/stores/:storeId/employees/:employeeId/edit", Controller.EditEmployee);
-// router.get("/stores/:storeId/employees/:employeeId/delete", Controller.deleteEmployee);
-// router.get("/employees", Controller.showDataEmployee);
+router.get("/stores/add", Controller.showForm);
+// router.post("/stores/add", Controller.addPackage);
+router.get("/stores/table/trx", Controller.showForm);
+router.post("/stores/table/trx", Controller.showForm);
+
 
 module.exports = router;
